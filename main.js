ece2596eef9253a80a49c6fcba525778cb3689b0
Dshane Godbold <dshanegodbold@gmail.com>
2022-02-16 09:32:31 -0600

    udated again lol

--- main.js
+++ main.js
@@ -1,13 +1,23 @@
 class pet {
-  constructor(species, name) {
+  constructor(species, name, color, eyes) {
     this.species = species;
     this.name = name;
+    this.color = color
+    this.eyes= eyes
+  }
+  describe (){
+      console.log(
+          `this is a ${this.species},`
+          + `my ${this.name} happens to be a ${this.species},`
+          +`his eyes are ${this.eyes} `
+          +`and his skin color is ${this.color}!`
+      );
   }
 }
 function main() {
-  const myPet = new pet("Wolf", "Jr.");
-  console.log(myPet.name);
-  console.log(myPet.species);
+  const myPet = new pet("Wolf", "Jr.", "Red", "Green");
+
+ myPet.describe()
 }
 
 main();
